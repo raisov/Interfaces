7844e1fbf85ab494207662258f82df7f53ec8e35
Vladimir Raisov <raisov@gmail.com>
2024-11-22 23:26:54 +0200

    refactor: simplify address extraction

--- Sources/RTInterface.swift
+++ Sources/RTInterface.swift
@@ -8,12 +8,27 @@ import Darwin.net.route
 
 /// A type describing network interface
 public struct RTInterface: Interface {
-    
+        
     private func withHeaderPointer<R>(_ body: (UnsafePointer<if_msghdr>) -> R) -> R {
         interfaceMessages.withUnsafeBytes {
             let p = $0.baseAddress!.assumingMemoryBound(to: if_msghdr.self)
             return body(p)
         }
+    }
+    
+    private func getAddresses(of kind: Int32) -> [sockaddr_storage] {
+        interfaceMessages.withUnsafeBytes {
+            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
+            return addressExtractor2(kind, interfaceMessages.count)(p)
+        }
+    }
+    
+    private func getIP4Addresses(of kind: Int32) -> [in_addr] {
+        getAddresses(of: kind).compactMap { $0.in?.sin_addr }
+    }
+    
+    private func getIP6Addresses(of kind: Int32) -> [in6_addr] {
+        getAddresses(of: kind).compactMap { $0.in6?.sin6_addr }
     }
 
     /// Contains routing messages with information about network interface
@@ -122,8 +137,8 @@ public struct RTInterface: Interface {
     ///     - count: size of the memory buffer
     /// - returns: specified function suitable for using as an argument for `withUnsafeBytes`
     /// - Warning: Don't try to undestand this function. It's dangerous for your peace of mind!
-    private func addressExtractor(of kind: Int32, _ count: Int) ->
-        (_ start: UnsafePointer<Int8>) -> [IPAddress] {
+    private func addressExtractor2(_ kind: Int32, _ count: Int) ->
+    (_ start: UnsafePointer<Int8>) -> [sockaddr_storage] {
 
             func sa_rlen<T>(_ x: T) -> Int where T: BinaryInteger {
                 return Int(x == 0 ? 4 : (x + 3) & ~3)
@@ -131,9 +146,9 @@ public struct RTInterface: Interface {
 
             assert(0 <= kind && kind < RTAX_MAX)
             let bitmask = Int32(1 << kind)
-            return {(_ start: UnsafePointer<Int8>) -> [IPAddress] in
+            return {(_ start: UnsafePointer<Int8>) -> [sockaddr_storage] in
                 let (index, length) = start.withMemoryRebound(to: if_msghdr.self, capacity: 1) {($0.pointee.ifm_index, $0.pointee.ifm_msglen)}
-                var addresses = [IPAddress]()
+                var addresses = [sockaddr_storage]()
                 var location = Int(length)
                 while location != count {
                     let rtm_p = start.advanced(by: location)
@@ -149,7 +164,7 @@ public struct RTInterface: Interface {
                         location += Int(length)
                         continue
                     }
-                    let ip = rtm_p.withMemoryRebound(to: ifa_msghdr.self, capacity: 1) {(ifam_p) -> IPAddress? in
+                    let address = rtm_p.withMemoryRebound(to: ifa_msghdr.self, capacity: 1) {(ifam_p) -> sockaddr_storage? in
                         guard ifam_p.pointee.ifam_index == index else {return nil}
                         var addrs = ifam_p.pointee.ifam_addrs
                         guard addrs & bitmask == bitmask else {return nil} // there is no address here
@@ -161,111 +176,64 @@ public struct RTInterface: Interface {
                             addrs >>= 1
                         }
                         let sa_p = p.bindMemory(to: sockaddr.self, capacity: 1)
-
-                        // Many, many years ago,
-                        // one programmer from Berkeley
-                        // decided to save a few bytes.
-                        // So now I had do the following ...
-                        if sa_p.pointee.sa_family == AF_INET && bitmask == RTA_NETMASK  {
-                            //                       When IPv6 was invented,
-                            //                       bytes have already become cheaper
-                            var sin = sockaddr_in()
-                            return withUnsafeMutablePointer(to: &sin) {
-                                let rp = UnsafeMutableRawPointer($0)
-                                rp.copyMemory(from: p, byteCount: Int(sa_p.pointee.sa_len))
-                                rp.assumingMemoryBound(to: sockaddr_in.self).pointee.sin_len = numericCast(MemoryLayout<sockaddr_in>.size)
-                                return rp.assumingMemoryBound(to: sockaddr_in.self).pointee.ip
-                            }
-                        } else {
-                            if let address = sa_p.internetAddress {
-                                if address.isWellFormed {
-                                    return address.ip
-                                }
-                            }
+                        assert(sa_p.pointee.sa_len <= SOCK_MAXADDRLEN, "malformed sockaddr")
+                        guard sa_p.pointee.sa_len <= SOCK_MAXADDRLEN else { return nil }
+                        assert(sa_p.pointee.sa_len > MemoryLayout<sockaddr>.offset(of: \.sa_data)!, "malformed sockaddr")
+                        guard sa_p.pointee.sa_len > MemoryLayout<sockaddr>.offset(of: \.sa_data)! else { return nil }
+                        var ss = sockaddr_storage()
+                        return withUnsafeMutablePointer(to: &ss) {
+                            let ss_p = UnsafeMutableRawPointer($0)
+                            ss_p.copyMemory(from: sa_p, byteCount: min(Int(sa_p.pointee.sa_len), Int(SOCK_MAXADDRLEN)))
+                            return ss_p.assumingMemoryBound(to: sockaddr_storage.self).pointee
                         }
-                        // I dislike this `continue`, `break` and `return` from the middle of the loop, too.
-                        // But what should I do with all these `withMemoryRebound`?
-                        return nil
                     }
-                    if let ip = ip {addresses.append(ip)}
+                    if let address {addresses.append(address)}
                     location += Int(length)
                 }
                 return addresses
             }
 
     }
-
-    /// Array of all IPv4 and IPv6 addresses of the interface.
-    public var addresses: [IPAddress] {
-        interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor(of: RTAX_IFA, interfaceMessages.count)(p)
-        }
-    }
     
     /// Array of all IPv4 addresses (including aliases) of the interface.
     public var ip4: [in_addr] {
-        interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor(of: RTAX_IFA, interfaceMessages.count)(p)
-                .filter{Swift.type(of: $0).family == .ip4}.compactMap{$0 as? in_addr}
-        }
+        getIP4Addresses(of: RTAX_IFA)
     }
     
     /// IPv4 network mask.
     public var mask4: in_addr? {
-        interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor(of: RTAX_NETMASK, interfaceMessages.count)(p)
-                .filter{Swift.type(of: $0).family == .ip4}.first as? in_addr
-        }
+        getIP4Addresses(of: RTAX_NETMASK).first
     }
-
 
     /// Array of all IPv6 addresses of the interface.
     public var ip6: [in6_addr] {
-        interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor(of: RTAX_IFA, interfaceMessages.count)(p)
-                .filter{Swift.type(of: $0).family == .ip6}.compactMap{$0 as? in6_addr}
-        }
+        getIP6Addresses(of: RTAX_IFA)
     }
 
     /// Array of all IPv6 network masks of the interface.
     /// - Note: The number and order of the masks correspond to the number and order
     /// of the ip6 addresses, so that they can be connected together using `zip` function.
-    /// ```
-    /// if let interface = (Interfaces().first{$0.ip6.count != 0}) {
-    ///    let addressesWithMasks = zip(interface.ip6, interface.masks6)
-    /// }
-    /// ```
     public var masks6: [in6_addr] {
-        interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor(of: RTAX_NETMASK, interfaceMessages.count)(p)
-                .filter{Swift.type(of: $0).family == .ip6}.compactMap{$0 as? in6_addr}
-        }
+        getIP6Addresses(of: RTAX_NETMASK)
     }
-
 
     /// Interface broadcast address, if applicable.
     public var broadcast: in_addr? {
         guard self.options.contains(.broadcast) else {return nil}
         guard !self.options.contains(.pointopoint) else {return nil}
-        return interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor(of: RTAX_BRD, interfaceMessages.count)(p)
-                .first{Swift.type(of: $0).family == .ip4} as? in_addr
-        }
+        return getIP4Addresses(of: RTAX_BRD).first
     }
 
-    /// For point-to-point interfaces, destination address.
-    public var destination: IPAddress? {
+    /// For point-to-point interfaces, destination IP4 address.
+    public var destination4: in_addr? {
         guard self.options.contains(.pointopoint) else {return nil}
-        return interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor(of: RTAX_BRD, interfaceMessages.count)(p).first
-        }
+        return getIP4Addresses(of: RTAX_BRD).first
+    }
+
+    /// For point-to-point interfaces, destination IP6 address.
+    public var destination6: in6_addr? {
+        guard self.options.contains(.pointopoint) else {return nil}
+        return getIP6Addresses(of: RTAX_BRD).first
     }
 }
 
