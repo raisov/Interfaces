967855d518a6fc7508b878844bc948d54aefffde
Vladimir Raisov <raisov@gmail.com>
2024-11-23 23:11:06 +0200

    chore: reorder RTInterface code, special processing for link-local ip6

--- Sources/RTInterface.swift
+++ Sources/RTInterface.swift
@@ -1,39 +1,17 @@
-//
 //  RTInterface.swift
-//  Interface2
+//  Interfaces package
+//  Copyright (c) 2018 Vladimir Raisov
+//  Licensed under MIT License
 import Sockets
 import Foundation
 #if canImport(Darwin.net.route)
 import Darwin.net.route
 
-/// A type describing network interface
 public struct RTInterface: Interface {
-        
-    private func withHeaderPointer<R>(_ body: (UnsafePointer<if_msghdr>) -> R) -> R {
-        interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: if_msghdr.self)
-            return body(p)
-        }
-    }
     
-    private func getAddresses(of kind: Int32) -> [sockaddr_storage] {
-        interfaceMessages.withUnsafeBytes {
-            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
-            return addressExtractor2(kind, interfaceMessages.count)(p)
-        }
-    }
-    
-    private func getIP4Addresses(of kind: Int32) -> [in_addr] {
-        getAddresses(of: kind).compactMap { $0.in?.sin_addr }
-    }
-    
-    private func getIP6Addresses(of kind: Int32) -> [in6_addr] {
-        getAddresses(of: kind).compactMap { $0.in6?.sin6_addr }
-    }
-
     /// Contains routing messages with information about network interface
     private let interfaceMessages: Data
-
+    
     /// Creates `Interface` as an element of `Interfaces` collection.
     /// - parameter interfaceMessages: part of the `sysctl` buffer with
     ///   information about the interface being created.
@@ -51,15 +29,15 @@ public struct RTInterface: Interface {
         }
         self.interfaceMessages = interfaceMessages
     }
-
-    ///
+    
+    // MARK: - Implementation of `Interface` protocol properties
+    
     public var index: Int32 {
         withHeaderPointer {
             numericCast($0.pointee.ifm_index)
         }
     }
-
-    /// BSD name of interface
+    
     public var name: String {
         withHeaderPointer {
             $0.advanced(by: 1).withMemoryRebound(to: sockaddr_dl.self, capacity: 1) {
@@ -67,65 +45,115 @@ public struct RTInterface: Interface {
             }
         }
     }
-
-    /// Hardware (link level) address of interface;
-    /// for ethernet - so-called MAC address.
-    public var link: [UInt8]? {
+    
+    public var link: [UInt8] {
         withHeaderPointer {
             $0.advanced(by: 1).withMemoryRebound(to: sockaddr_dl.self, capacity: 1) {
-                guard $0.address.count != 0 else {return nil}
-                return $0.address
+                $0.address
             }
         }
     }
-
-    /// True, if it is possible to work with interface as with ethernet;
-    /// for example, Wi-Fi interface is ethernet compatible.
+    
     public var isEthernetCompatible: Bool {
         withHeaderPointer {
             $0.advanced(by: 1).withMemoryRebound(to: sockaddr_dl.self, capacity: 1) {
                 $0.type == IFT_ETHER
             }
         }
-    }
-
-
-    /// That's it, the type of interface.
+    }    
+    
     public var type: InterfaceType {
         withHeaderPointer {
             InterfaceType(Int32($0.pointee.ifm_data.ifi_type))
         }
     }
-    
-
-    /// This interface options.
+        
     public var options: InterfaceOptions {
         withHeaderPointer {
             InterfaceOptions(rawValue: $0.pointee.ifm_flags)
         }
     }
-
-    /// Maximum Transmission Unit size for interface.
-    public var mtu: Int {
+    public var mtu: UInt32 {
         withHeaderPointer {
-            Int($0.pointee.ifm_data.ifi_mtu)
+            $0.pointee.ifm_data.ifi_mtu
         }
     }
-
-    /// Network routing metric.
-    public var metric: Int {
+    
+    public var metric: UInt32 {
         withHeaderPointer {
-            Int($0.pointee.ifm_data.ifi_metric)
+            $0.pointee.ifm_data.ifi_metric
         }
     }
-
-    /// Possible link speed; may be 0 if undefined.
-    public var baudrate: Int {
+    
+    public var baudrate: UInt32 {
         withHeaderPointer {
-            Int($0.pointee.ifm_data.ifi_baudrate)
+            $0.pointee.ifm_data.ifi_baudrate
         }
     }
+    
+    public var ip4: [in_addr] {
+        getIP4Addresses(of: RTAX_IFA)
+    }
+    
+    public var mask4: in_addr? {
+        getIP4Addresses(of: RTAX_NETMASK).first
+    }
+    
+    public var ip6: [in6_addr] {
+        getIP6Addresses(of: RTAX_IFA)
+    }
+    
+    public var masks6: [in6_addr] {
+        getIP6Addresses(of: RTAX_NETMASK)
+    }
+    
+    public var broadcast: in_addr? {
+        guard self.options.contains(.broadcast) else {return nil}
+        guard !self.options.contains(.pointopoint) else {return nil}
+        return getIP4Addresses(of: RTAX_BRD).first
+    }
+    
+    public var destination4: in_addr? {
+        guard self.options.contains(.pointopoint) else {return nil}
+        return getIP4Addresses(of: RTAX_BRD).first
+    }
+    
+    public var destination6: in6_addr? {
+        guard self.options.contains(.pointopoint) else {return nil}
+        return getIP6Addresses(of: RTAX_BRD).first
+    }
 
+    // MARK: - Private methods
+    
+    private func withHeaderPointer<R>(_ body: (UnsafePointer<if_msghdr>) -> R) -> R {
+        interfaceMessages.withUnsafeBytes {
+            let p = $0.baseAddress!.assumingMemoryBound(to: if_msghdr.self)
+            return body(p)
+        }
+    }
+    
+    private func getAddresses(of kind: Int32) -> [sockaddr_storage] {
+        interfaceMessages.withUnsafeBytes {
+            let p = $0.baseAddress!.assumingMemoryBound(to: Int8.self)
+            return addressExtractor(kind, interfaceMessages.count)(p)
+        }
+    }
+    
+    private func getIP4Addresses(of kind: Int32) -> [in_addr] {
+        getAddresses(of: kind).compactMap { $0.in?.sin_addr }
+    }
+    
+    private func getIP6Addresses(of kind: Int32) -> [in6_addr] {
+        getAddresses(of: kind).compactMap {
+            guard var ip6 = $0.in6?.sin6_addr else { return nil }
+            if ip6.isLinkLocal {
+                ip6.__u6_addr.__u6_addr32.0 &= 0x80fe
+                ip6.__u6_addr.__u6_addr32.1 = 0
+            }
+            return ip6
+        }
+    }
+    
     /// This is a curried function intended to produce a function
     /// for retrieving data of given `kind` from the memory buffer
     /// containing `RTM_NEWADDR` type routing messages.
@@ -137,110 +165,69 @@ public struct RTInterface: Interface {
     ///     - count: size of the memory buffer
     /// - returns: specified function suitable for using as an argument for `withUnsafeBytes`
     /// - Warning: Don't try to undestand this function. It's dangerous for your peace of mind!
-    private func addressExtractor2(_ kind: Int32, _ count: Int) ->
+    private func addressExtractor(_ kind: Int32, _ count: Int) ->
     (_ start: UnsafePointer<Int8>) -> [sockaddr_storage] {
-
-            func sa_rlen<T>(_ x: T) -> Int where T: BinaryInteger {
-                return Int(x == 0 ? 4 : (x + 3) & ~3)
-            }
-
-            assert(0 <= kind && kind < RTAX_MAX)
-            let bitmask = Int32(1 << kind)
-            return {(_ start: UnsafePointer<Int8>) -> [sockaddr_storage] in
-                let (index, length) = start.withMemoryRebound(to: if_msghdr.self, capacity: 1) {($0.pointee.ifm_index, $0.pointee.ifm_msglen)}
-                var addresses = [sockaddr_storage]()
-                var location = Int(length)
-                while location != count {
-                    let rtm_p = start.advanced(by: location)
-                    let (version, type, length) = rtm_p.withMemoryRebound(to: rt_msghdr.self, capacity: 1) {
-                        ($0.pointee.rtm_version, $0.pointee.rtm_type, $0.pointee.rtm_msglen)
-                    }
-                    guard version == RTM_VERSION else {
-                        location += Int(length)
-                        continue
-                    }
-                    if type == RTM_IFINFO {break}
-                    guard type == RTM_NEWADDR else {
-                        location += Int(length)
-                        continue
-                    }
-                    let address = rtm_p.withMemoryRebound(to: ifa_msghdr.self, capacity: 1) {(ifam_p) -> sockaddr_storage? in
-                        guard ifam_p.pointee.ifam_index == index else {return nil}
-                        var addrs = ifam_p.pointee.ifam_addrs
-                        guard addrs & bitmask == bitmask else {return nil} // there is no address here
-                        var p = UnsafeRawPointer(ifam_p.advanced(by: 1))
-                        for _ in 0..<kind {
-                            if addrs & 1 != 0 {
-                                p += sa_rlen(p.bindMemory(to: sockaddr.self, capacity: 1).pointee.sa_len)
-                            }
-                            addrs >>= 1
-                        }
-                        let sa_p = p.bindMemory(to: sockaddr.self, capacity: 1)
-                        assert(sa_p.pointee.sa_len <= SOCK_MAXADDRLEN, "malformed sockaddr")
-                        guard sa_p.pointee.sa_len <= SOCK_MAXADDRLEN else { return nil }
-                        assert(sa_p.pointee.sa_len > MemoryLayout<sockaddr>.offset(of: \.sa_data)!, "malformed sockaddr")
-                        guard sa_p.pointee.sa_len > MemoryLayout<sockaddr>.offset(of: \.sa_data)! else { return nil }
-                        var ss = sockaddr_storage()
-                        return withUnsafeMutablePointer(to: &ss) {
-                            let ss_p = UnsafeMutableRawPointer($0)
-                            ss_p.copyMemory(from: sa_p, byteCount: min(Int(sa_p.pointee.sa_len), Int(SOCK_MAXADDRLEN)))
-                            return ss_p.assumingMemoryBound(to: sockaddr_storage.self).pointee
-                        }
-                    }
-                    if let address {addresses.append(address)}
-                    location += Int(length)
+        
+        func sa_rlen<T>(_ x: T) -> Int where T: BinaryInteger {
+            return Int(x == 0 ? 4 : (x + 3) & ~3)
+        }
+        
+        assert(0 <= kind && kind < RTAX_MAX)
+        let bitmask = Int32(1 << kind)
+        return {(_ start: UnsafePointer<Int8>) -> [sockaddr_storage] in
+            let (index, length) = start.withMemoryRebound(to: if_msghdr.self, capacity: 1) {($0.pointee.ifm_index, $0.pointee.ifm_msglen)}
+            var addresses = [sockaddr_storage]()
+            var location = Int(length)
+            while location != count {
+                let rtm_p = start.advanced(by: location)
+                let (version, type, length) = rtm_p.withMemoryRebound(to: rt_msghdr.self, capacity: 1) {
+                    ($0.pointee.rtm_version, $0.pointee.rtm_type, $0.pointee.rtm_msglen)
                 }
-                return addresses
+                guard version == RTM_VERSION else {
+                    location += Int(length)
+                    continue
+                }
+                if type == RTM_IFINFO {break}
+                guard type == RTM_NEWADDR else {
+                    location += Int(length)
+                    continue
+                }
+                let address = rtm_p.withMemoryRebound(to: ifa_msghdr.self, capacity: 1) {(ifam_p) -> sockaddr_storage? in
+                    guard ifam_p.pointee.ifam_index == index else {return nil}
+                    var addrs = ifam_p.pointee.ifam_addrs
+                    guard addrs & bitmask == bitmask else {return nil} // there is no address here
+                    var p = UnsafeRawPointer(ifam_p.advanced(by: 1))
+                    for _ in 0..<kind {
+                        if addrs & 1 != 0 {
+                            p += sa_rlen(p.bindMemory(to: sockaddr.self, capacity: 1).pointee.sa_len)
+                        }
+                        addrs >>= 1
+                    }
+                    let sa_p = p.bindMemory(to: sockaddr.self, capacity: 1)
+                    assert(sa_p.pointee.sa_len <= SOCK_MAXADDRLEN, "malformed sockaddr")
+                    guard sa_p.pointee.sa_len <= SOCK_MAXADDRLEN else { return nil }
+                    guard sa_p.pointee.sa_len > MemoryLayout<sockaddr>.offset(of: \.sa_data)! else { return nil }
+                    var ss = sockaddr_storage()
+                    withUnsafeMutablePointer(to: &ss) {
+                        let ss_p = UnsafeMutableRawPointer($0)
+                        ss_p.copyMemory(from: sa_p, byteCount: min(Int(sa_p.pointee.sa_len), Int(SOCK_MAXADDRLEN)))
+                        return ss_p.assumingMemoryBound(to: sockaddr_storage.self).pointee
+                    }
+                    return ss
+                }
+                if let address {addresses.append(address)}
+                location += Int(length)
             }
-
-    }
-    
-    /// Array of all IPv4 addresses (including aliases) of the interface.
-    public var ip4: [in_addr] {
-        getIP4Addresses(of: RTAX_IFA)
-    }
-    
-    /// IPv4 network mask.
-    public var mask4: in_addr? {
-        getIP4Addresses(of: RTAX_NETMASK).first
-    }
-
-    /// Array of all IPv6 addresses of the interface.
-    public var ip6: [in6_addr] {
-        getIP6Addresses(of: RTAX_IFA)
-    }
-
-    /// Array of all IPv6 network masks of the interface.
-    /// - Note: The number and order of the masks correspond to the number and order
-    /// of the ip6 addresses, so that they can be connected together using `zip` function.
-    public var masks6: [in6_addr] {
-        getIP6Addresses(of: RTAX_NETMASK)
-    }
-
-    /// Interface broadcast address, if applicable.
-    public var broadcast: in_addr? {
-        guard self.options.contains(.broadcast) else {return nil}
-        guard !self.options.contains(.pointopoint) else {return nil}
-        return getIP4Addresses(of: RTAX_BRD).first
-    }
-
-    /// For point-to-point interfaces, destination IP4 address.
-    public var destination4: in_addr? {
-        guard self.options.contains(.pointopoint) else {return nil}
-        return getIP4Addresses(of: RTAX_BRD).first
-    }
-
-    /// For point-to-point interfaces, destination IP6 address.
-    public var destination6: in6_addr? {
-        guard self.options.contains(.pointopoint) else {return nil}
-        return getIP6Addresses(of: RTAX_BRD).first
+            return addresses
+        }
+        
     }
 }
 
 public struct RTInterfaces: Collection {
     public typealias Element = Interface
 
-    /// Contains `sysctl` results
+    /// Contains `RTM_NEWADDR` type routing messages from `sysctl` results
     let routingMessages: Data
 
     public init() {
@@ -267,12 +254,33 @@ public struct RTInterfaces: Collection {
     }
     
 /// The following are necessary to ensure conformance `Collection` protocol.
-
     public struct Index: Comparable {
         fileprivate let value: Int
         public static func == (lhs: Index, rhs: Index) -> Bool {return lhs.value == rhs.value}
         public static func < (lhs: Index, rhs: Index) -> Bool {return lhs.value < rhs.value}
         fileprivate init(_ value: Int) {self.value = value}
+    }
+
+    public var endIndex: Index {return Index(routingMessages.endIndex)}
+    public var startIndex: Index {return nextIndex(from: routingMessages.startIndex)}
+
+    public func index(after given: Index) -> Index {
+        return routingMessages.suffix(from: given.value).withUnsafeBytes {
+            let ifm_p = $0.baseAddress!.assumingMemoryBound(to: if_msghdr.self)
+            assert(ifm_p.pointee.ifm_version == RTM_VERSION)
+            assert(ifm_p.pointee.ifm_type == RTM_IFINFO)
+            assert(ifm_p.pointee.ifm_addrs & RTA_IFP != 0)
+            assert(ifm_p.pointee.ifm_index != 0)
+            ifm_p.advanced(by: 1).withMemoryRebound(to: sockaddr_dl.self, capacity: 1) {
+                assert($0.pointee.isWellFormed)
+                assert($0.index == ifm_p.pointee.ifm_index)
+            }
+            return nextIndex(from: given.value + Int(ifm_p.pointee.ifm_msglen))
+        }
+    }
+
+    public subscript(position: Index) -> Element {
+        return RTInterface(routingMessages.subdata(in: position.value..<index(after: position).value))
     }
 
     private func nextIndex(from indexValue: Int) -> Index {
@@ -299,28 +307,6 @@ public struct RTInterfaces: Collection {
             }
             return Index(location)
         }
-    }
-
-    public var endIndex: Index {return Index(routingMessages.endIndex)}
-    public var startIndex: Index {return nextIndex(from: routingMessages.startIndex)}
-
-    public func index(after given: Index) -> Index {
-        return routingMessages.suffix(from: given.value).withUnsafeBytes {
-            let ifm_p = $0.baseAddress!.assumingMemoryBound(to: if_msghdr.self)
-            assert(ifm_p.pointee.ifm_version == RTM_VERSION)
-            assert(ifm_p.pointee.ifm_type == RTM_IFINFO)
-            assert(ifm_p.pointee.ifm_addrs & RTA_IFP != 0)
-            assert(ifm_p.pointee.ifm_index != 0)
-            ifm_p.advanced(by: 1).withMemoryRebound(to: sockaddr_dl.self, capacity: 1) {
-                assert($0.pointee.isWellFormed)
-                assert($0.index == ifm_p.pointee.ifm_index)
-            }
-            return nextIndex(from: given.value + Int(ifm_p.pointee.ifm_msglen))
-        }
-    }
-
-    public subscript(position: Index) -> Element {
-        return RTInterface(routingMessages.subdata(in: position.value..<index(after: position).value))
     }
 }
 #endif
